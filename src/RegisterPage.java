53d0227cff5255912fcfd66fedac19e17d703398
bipulkmr-crypto <72005385+bipulkmr-crypto@users.noreply.github.com>
2021-11-13 15:38:23 +0530

    Initial commit

---
+++ src/RegisterPage.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class RegisterPage extends JFrame {
+    public RegisterPage()
+    {
+        super("register");
+        this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    public  static void main(String[] args)
+    {
+        RegisterPage obj=new RegisterPage();
+    }
+
+
+}
